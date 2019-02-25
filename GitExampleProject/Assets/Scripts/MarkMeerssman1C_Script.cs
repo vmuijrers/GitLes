fa329a4b48d0f3f07b5a5e80f2e83bad80c6a473
Markreel <markmeerssman@live.nl>
2019-02-25 15:11:59 +0100

    Lekker mannetje

---
+++ GitExampleProject/Assets/Scripts/MarkMeerssman1C_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkMeerssman1C_Script : MonoBehaviour
+{
+    float ranzigTimertje = 5;
+
+    void Update()
+    {
+       if(ranzigTimertje >= 0)
+        {
+            ranzigTimertje -= Time.deltaTime;
+        }
+       else
+        {
+            RandomJump();
+            ranzigTimertje = 5;
+        }
+    }
+
+    void RandomJump()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * Random.Range(500, 750));    
+    }
+}
